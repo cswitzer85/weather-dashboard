ac03175e0983d314e4f1fdd50207fdea5203b5b0
Cody Switzer <cswitzer85@gmail.com>
2020-02-03 21:01:42 -0600

    API calls work in console, function not working yet, linked moment to HTML doc and adjusted css for search area

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 //Weather API key: f3d9d80cd57331f1adc06438f8df083c
 
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
+//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={f3d9d80cd57331f1adc06438f8df083c}
 
+"https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
+var cityInput = document.getElementById("cityNameInput").textContent;
+
+function getWeather() {
+    $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413",
+            method: "GET"
+        })
+        .then(function (response) {
+            console.log(response);
+        });
+}
+
+// $.ajax({
+//     url: "https://api.openweathermap.org/data/2.5/weather?q=detroit&units=imperial&appid=166a433c57516f51dfab1f7edaed8413",
+//     method: "GET"
+//   })
+//   .then(function(response) {
+//     console.log(response);
+//   });  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
