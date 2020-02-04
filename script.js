1a448761a444d9cfe8abd843368338d7de6a2e35
Cody Switzer <cswitzer85@gmail.com>
2020-02-03 23:21:26 -0600

    API call works in console when search button clicked

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-//Weather API key: f3d9d80cd57331f1adc06438f8df083c
+var cityInput = "";
+var pastSearches = [];
+var city = response.name;
+var temp = response.temp;
+var weather = response.weather;
+var humidity = response.main.humidity;
 
+//Weather API key: f3d9d80cd57331f1adc06438f8df083c
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={f3d9d80cd57331f1adc06438f8df083c}
 
-"https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
-var cityInput = document.getElementById("cityNameInput").textContent;
-
 function getWeather() {
+    cityInput = document.getElementById("cityNameInput").value;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413";
     $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413",
+            url: queryURL,
             method: "GET"
         })
         .then(function (response) {
             console.log(response);
-        });
-}
+            // var city = response.name;
+            console.log(city);
+            // var temp = response.temp;
+            consonsle.log(temp);
+            // var weather = response.weather;
+            console.log(weather);
+            // var humidity = response.main.humidity;
+            console.log(humidity);
 
-// $.ajax({
-//     url: "https://api.openweathermap.org/data/2.5/weather?q=detroit&units=imperial&appid=166a433c57516f51dfab1f7edaed8413",
-//     method: "GET"
-//   })
-//   .then(function(response) {
-//     console.log(response);
-//   });  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+
+        });
+
+}
