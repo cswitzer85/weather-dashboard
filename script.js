c88805229e1bfcb00bbfdfc384021d24fc3c1584
Cody Switzer <cswitzer85@gmail.com>
2020-02-04 20:45:41 -0600

    search input saves city to local storage and creates a button with city name

--- script.js
+++ script.js
@@ -12,18 +12,17 @@ var pastSearches = [];
 // displaySearchHistory();
 
 
-// function displaySearchHistory(){
-//         $("#searchHistory").empty();
+function displaySearchHistory(){
+        $("#searchHistory").empty();
 
-//         for (var i = 0; i < pastSearches.length; i++) {
-//           var a = $("<button>");
-//           a.addClass("pastCities");
-//           a.attr("data-name", pastSearches[i]);
-//           a.text(pastSearches[i]);
-//           $("#buttons-view").append(a);
-//         }
-//       }
-//   $(document).on("click", ".pastCities", FuNcTiOn); will need to set value of city input to 
+        for (var i = 0; i < pastSearches.length; i++) {
+          var a = $("<button>");
+          a.addClass("pastCities");
+          a.attr("data-name", pastSearches[i]);
+          a.text(pastSearches[i]);
+          $("#searchHistory").append(a);
+        }
+      }
 
 // var frisco = $.ajax({ url: "https://api.openweathermap.org/data/2.5/weather?q=frisco&units=imperial&appid=166a433c57516f51dfab1f7edaed8413", method: "GET"}) .then(function (response) {console.log(response);})
 
@@ -36,7 +35,8 @@ function getWeather() {
             method: "GET"
         })
         .then(function (response) {
-            var currentWeatherDiv = $("<div.movie>");
+        
+            var currentWeatherDiv = $("<div.current>");
             var city = response.name;
             pName = $("<p>").text("City: " + city)
             currentWeatherDiv.append(pName)
@@ -47,6 +47,19 @@ function getWeather() {
             currentWeatherDiv.append(ptemp)
             $("#currentWeather").append(currentWeatherDiv)
 
+            cityButtonName = response.name;
+            localStorage.setItem(cityButtonName, "https://api.openweathermap.org/data/2.5/weather?q=" + cityButtonName + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413");
+            var cityButtonURL = localStorage.getItem(cityButtonName.toString())
+            var historicWeatherDiv = $("<div.historic>");
+            var city = response.name;
+            buttonName = $("<button>").text(city)
+            historicWeatherDiv.append(buttonName)
+            breakElement = $("<br>")
+            $("#searchHistory").append(historicWeatherDiv)
+            $("#searchHistory").append(breakElement)
+
+
+        
 
             // var temp = response.temp;
             // consonsle.log(temp);
@@ -63,3 +76,19 @@ function getWeather() {
 }
 
 // $("#currentWeather").append(currentWeatherDiv)
+// $.ajax({
+//     url: "https://api.openweathermap.org/data/2.5/weather?q=frisco&units=imperial&appid=166a433c57516f51dfab1f7edaed8413",
+//     method: "GET"
+// })
+// .then(function (response) {
+//     console.log("city save")
+//     cityButtonName = response.name;
+//     localStorage.setItem(cityButtonName, "https://api.openweathermap.org/data/2.5/weather?q=" + cityButtonName + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413");
+//     var cityButtonURL = localStorage.getItem(cityButtonName.toString())
+//     var historicWeatherDiv = $("<div.historic>");
+//     var city = response.name;
+//     buttonName = $("<button>").text(city)
+//     historicWeatherDiv.append(buttonName)
+//     $("#searchHistory").append(historicWeatherDiv)
+
+// })
