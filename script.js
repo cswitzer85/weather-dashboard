cf4a6a7f4165e9a3dfd816e8bb070f2b7f3a58a8
Cody Switzer <cswitzer85@gmail.com>
2020-02-04 22:53:17 -0600

    API call renders UV index and 5 day forecast data to screen

--- script.js
+++ script.js
@@ -11,18 +11,36 @@ var pastSearches = [];
 // pastSearches.push(setCity)
 // displaySearchHistory();
 
+// function getUV (){
+// var lat = response.coord.lon
+// var lon = response.coord.lat
+// $.ajax({
+//     url: "http://api.openweathermap.org/data/2.5/uvi/history?appid=166a433c57516f51dfab1f7edaed8413&lat=" + lat + "&lon= + lon",
+//     method: "GET"
+// })
+// .then(function (response) {
+// uv = response.value;
+//     pUV = $("<p>").text("UV index: " + UV)
+//     currentWeatherDiv.append(pUV)
+//     $("#currentWeather").append(currentWeatherDiv)
+// })}
 
-function displaySearchHistory(){
-        $("#searchHistory").empty();
+// function displaySearchHistory(){
+//         $("#searchHistory").empty();
 
-        for (var i = 0; i < pastSearches.length; i++) {
-          var a = $("<button>");
-          a.addClass("pastCities");
-          a.attr("data-name", pastSearches[i]);
-          a.text(pastSearches[i]);
-          $("#searchHistory").append(a);
-        }
-      }
+//         for (var i = 0; i < pastSearches.length; i++) {
+//           var a = $("<button>");
+//           a.addClass("pastCities");
+//           a.attr("data-name", pastSearches[i]);
+//           a.text(pastSearches[i]);
+//           $("#searchHistory").append(a);
+//         }
+//       }
+
+function clearHist() {
+    localStorage.clear();
+    $("#searchHistory").empty();
+}
 
 // var frisco = $.ajax({ url: "https://api.openweathermap.org/data/2.5/weather?q=frisco&units=imperial&appid=166a433c57516f51dfab1f7edaed8413", method: "GET"}) .then(function (response) {console.log(response);})
 
@@ -35,17 +53,71 @@ function getWeather() {
             method: "GET"
         })
         .then(function (response) {
-        
+
             var currentWeatherDiv = $("<div.current>");
             var city = response.name;
             pName = $("<p>").text("City: " + city)
             currentWeatherDiv.append(pName)
             $("#currentWeather").append(currentWeatherDiv)
 
+
             var temp = response.main.temp;
-            ptemp = $("<p>").text("Temp: " + temp)
+            ptemp = $("<p>").text("Temperature: " + temp)
             currentWeatherDiv.append(ptemp)
             $("#currentWeather").append(currentWeatherDiv)
+
+            var humidity = response.main.humidity;
+            pHumidity = $("<p>").text("Humidity: " + humidity)
+            currentWeatherDiv.append(pHumidity)
+            $("#currentWeather").append(currentWeatherDiv)
+
+            var wind = response.wind.speed;
+            pWind = $("<p>").text("Wind: " + wind)
+            currentWeatherDiv.append(pWind)
+            $("#currentWeather").append(currentWeatherDiv)
+
+            // var weatherDescription = response.weather[0].main;
+            // var weatherDescription = response.weather[0].description;
+            // var weatherDescription = response.weather[0].icon;
+
+            //reformat unix time
+            // var unix_timestamp = 1580876826
+            // var date = new Date(unix_timestamp * 1000);
+            // var hours = date.getHours();
+            // var minutes = "0" + date.getMinutes();
+            // var seconds = "0" + date.getSeconds();
+
+            // var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+
+            var lat = response.coord.lat
+            var lon = response.coord.lon
+
+            //UV API call
+            $.ajax({
+                    url: "http://api.openweathermap.org/data/2.5/uvi?appid=166a433c57516f51dfab1f7edaed8413&lat=" + lat + "&lon=" + lon,
+                    method: "GET"
+                })
+                .then(function (response) {
+                    UV = response.value;
+                    pUV = $("<p>").text("UV index: " + UV)
+                    currentWeatherDiv.append(pUV)
+                    $("#currentWeather").append(currentWeatherDiv)
+                })
+            // 5 day API call
+
+            $.ajax({
+                    url: "http://api.openweathermap.org/data/2.5/forecast?appid=166a433c57516f51dfab1f7edaed8413&lat=" + lat + "&lon=" + lon,
+                    method: "GET"
+                })
+                .then(function (response) {
+                    fiveDay = response;
+                    pfiveDay = $("<p>").text("Five Day Forecast: " + fiveDay)
+                    currentWeatherDiv.append(pfiveDay)
+                    $("#currentWeather").append(currentWeatherDiv)
+                })
+
+
 
             cityButtonName = response.name;
             localStorage.setItem(cityButtonName, "https://api.openweathermap.org/data/2.5/weather?q=" + cityButtonName + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413");
@@ -59,36 +131,15 @@ function getWeather() {
             $("#searchHistory").append(breakElement)
 
 
-        
-
-            // var temp = response.temp;
-            // consonsle.log(temp);
-
-            // var weather = response.weather;
-            // console.log(weather);
-
-            // var humidity = response.main.humidity;
-            // console.log(humidity);
-
-
         });
-
 }
 
-// $("#currentWeather").append(currentWeatherDiv)
-// $.ajax({
-//     url: "https://api.openweathermap.org/data/2.5/weather?q=frisco&units=imperial&appid=166a433c57516f51dfab1f7edaed8413",
-//     method: "GET"
-// })
-// .then(function (response) {
-//     console.log("city save")
-//     cityButtonName = response.name;
-//     localStorage.setItem(cityButtonName, "https://api.openweathermap.org/data/2.5/weather?q=" + cityButtonName + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413");
-//     var cityButtonURL = localStorage.getItem(cityButtonName.toString())
-//     var historicWeatherDiv = $("<div.historic>");
-//     var city = response.name;
-//     buttonName = $("<button>").text(city)
-//     historicWeatherDiv.append(buttonName)
-//     $("#searchHistory").append(historicWeatherDiv)
+// let unix_timestamp = 1549312452
+// var date = new Date(unix_timestamp * 1000);
+// var hours = date.getHours();
+// var minutes = "0" + date.getMinutes();
+// var seconds = "0" + date.getSeconds();
 
-// })
+// var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+// console.log(formattedTime);
